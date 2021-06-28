e019ee405bce9a6a7290db92eaeefbf090efe09a
KimalIsaev <kimchikumnik@gmail.com>
2021-06-28 19:13:52 +0700

    firsy commit

---
+++ frm.h
@@ -0,0 +1,8 @@
+#ifndef IMITATION_FORMULAS
+#define IMITATION_FORMULAS
+#include "math.h"
+
+double exp_dist(double a, double l);
+double hyperexp_dist(double a, double q, unsigned int n, double* v_q, double* v_l );
+
+#endif /* IMITATION_FORMULAS */
