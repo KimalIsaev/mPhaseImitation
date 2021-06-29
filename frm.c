646f29369d18c3ad0361a591580905cf0ec36d84
KimalIsaev <kimchikumnik@gmail.com>
2021-06-29 12:49:49 +0700

    added hyperexponential distribution

--- frm.c
+++ frm.c
@@ -4,7 +4,21 @@ double exp_dist(double a, double l){
 	return -log(1-a)/l; 
 }
 
-double hyperexp2(double a, double p, double q, double l1, double l2){
-	return exp_dist(a, (p < q) ? l1 : l2);
+double hyperexp_dist(double a, double p, unsigned int n,
+		double* v_q, double* v_l){
+	unsigned int i;
+	for(i=0; i < n - 1; i++)
+		if (v_q[i] > p)
+			break;
+	return exp_dist(a, v_l[i]);
+
+}
+
+void get_consecative_sums(double* original, unsigned int n, double* sums){
+	double s = 0;
+	for(unsigned int i = 0; i < n; i++){
+		S += original[i];
+		sums[i] = S;
+	}	
 }
 
--- frm.h
+++ frm.h
@@ -3,6 +3,8 @@
 #include "math.h"
 
 double exp_dist(double a, double l);
-double hyperexp_dist(double a, double q, unsigned int n, double* v_q, double* v_l );
+double hyperexp_dist(double a, double q, unsigned int n, 
+		double* v_q, double* v_l );
+void get_consecative_sums(double* original, unsigned int n, double* sums);
 
 #endif /* IMITATION_FORMULAS */
