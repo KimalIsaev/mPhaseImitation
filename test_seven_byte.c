6d63733f47f65304e543bf8d65a72cf87154c24d
KimalIsaev <kimchikumnik@gmail.com>
2021-06-29 16:05:36 +0700

    added test for showing how seven bytes is copied into unsigned long long

--- test_seven_byte.c
+++ test_seven_byte.c
@@ -1,5 +1,18 @@
-#include <string>
+#include <string.h>
+#include <stdio.h>
 
 int main(){
-
+	char seven_byte[7];
+	seven_byte[0] = 1;
+	seven_byte[1] = 0;
+	seven_byte[2] = 0;
+	seven_byte[3] = 0;
+	seven_byte[4] = 0;
+	seven_byte[5] = 0;
+	seven_byte[6] = 1;
+	unsigned long long ull;
+	memcpy(&ull, seven_byte, 7);
+	unsigned int ui = ull;
+	printf("%d\n", ui);
+	return 0;
 }
