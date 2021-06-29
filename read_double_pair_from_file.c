5ae45f041ff48cd4ed49b11aae491dff83a13b43
KimalIsaev <kimchikumnik@gmail.com>
2021-06-30 03:30:58 +0700

    rewritten function for reading files with random numbers for reading bytes not symbols, test in the same file

--- read_double_pair_from_file.c
+++ read_double_pair_from_file.c
@@ -1,4 +1,22 @@
 #include "read_double_pair_from_file.h"
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+void printf_ull(unsigned long long ull){
+	printf("%x %x\n", (unsigned int) (ull >> 32), (unsigned int) ull);
+}
+*/
+const char SEVEN_BYTE_SIZE = 7;
+const char FOURTEEN_BYTE_SIZE = 14;
+
+unsigned int fsize(FILE *fp){
+	unsigned int prev = ftell(fp);
+	fseek(fp, 0L, SEEK_END);
+	unsigned int sz = ftell(fp);
+	fseek(fp, prev, SEEK_SET);
+	return sz;
+}
 
 static double random_ull_to_random_double(unsigned long long ull)
 {
@@ -8,36 +26,38 @@ static double random_ull_to_random_double(unsigned long long ull)
 		unsigned long long integer;
 		double rational;
 	} transition;
-	transition.integer = ull;
-	return DOUBLE_ZEROS & (DOUBLE_ONES | transition.double);
+	transition.integer = DOUBLE_ZEROS & (DOUBLE_ONES | ull);
+	return transition.rational - 1;
 }
-
-
 
 unsigned int file_to_array(char* filename, struct double_pair** array){
 	FILE *fp;
-	char *line = NULL;
-	char *space = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned long long high, low;
-	unsigned int count = 0;
 	fp = fopen(filename, "r");
 	if (fp == NULL) return 0;
-	while ((read = getline(&line, &len, fp)) > 0) count++;
+	unsigned int file_size = fsize(fp);
+	//printf("%d\n", file_size);
+	unsigned int number_of_double_pairs = (file_size - 1) 
+		/ FOURTEEN_BYTE_SIZE; //last symbol is end symbol 
+	*array = malloc(sizeof(struct double_pair)*number_of_double_pairs);
+	unsigned long long buffer = 0;
+	for(unsigned int i = 0; i < number_of_double_pairs; i++){
+		fread(&buffer, SEVEN_BYTE_SIZE, 1, fp);
+		//printf_ull(buffer);
+		(*array)[i].first = random_ull_to_random_double(buffer);
+		fread(&buffer, SEVEN_BYTE_SIZE, 1, fp);
+		//printf_ull(buffer);
+		(*array)[i].second = random_ull_to_random_double(buffer);
+	}   
+	
 	fclose(fp);
-
-	fp = fopen(filename, "r");
-	*array = malloc(sizeof(struct double_pair)*count);
-	for (long long i = 0; i < count; i++){
-		read = getline(&line, &len, fp);
-		high = strtoull(line, &space, 16);
-		low = strtoull(++space, NULL,  16);
-		(*array)[i].first = random_ull_to_random_double(high);
-		(*array)[i].second = random_ull_to_random_double(low);
-	}
-
-	free(line);
-	fclose(fp);
-	return count;
+	return number_of_double_pairs;
 }
+
+int main(){
+	char filename[] = "test.txt";
+	struct double_pair* double_pairs;
+	file_to_array(filename, &double_pairs);
+	return 0;
+}
+
+
